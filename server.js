295f9986fb816cb3dd47537a5a2bbacef6bdbf98
Sue Lee <sue.lee0308@gmail.com>
2021-11-12 22:40:50 -0600

    deploying

--- server.js
+++ server.js
@@ -14,8 +14,15 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
-
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/Workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
   });
